7b303187fadf0c93927d792fd1d6c0787c7180c3
Jon Parsons <jon.parsons@guardian.co.uk>
2014-11-18 17:06:08 +0000

    added postinstall to copy a generic run-tests.sh script into project root (scribe-plugin-*)

---
+++ bin/postinstall.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# POST INSTALL
+# This script will copy boilerplate scripts into the current working directory
+# It is assumed that this module is installed as a dependency to a scribe plug-in
+
+CWD=`pwd`;
+DIRNAME=`dirname $0`;
+FILENAME=`basename $0`;
+FILEPATH="$DIRNAME/$FILENAME";
+
+if [ ! -e "$CWD/run-tests.sh"  ]; then
+  cp "$DIRNAME/run-tests.sh"  "$CWD/run-tests.sh";
+fi
