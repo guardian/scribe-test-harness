0e3761dae2c288043c2dca1cf3b4948f46e5fdba
Robert Rees <robert.rees@guardian.co.uk>
2014-11-20 17:00:58 +0000

    Copies to the script *above* the node_modules location

--- bin/postinstall.sh
+++ bin/postinstall.sh
@@ -4,9 +4,12 @@
 # This script will copy boilerplate scripts into the current working directory
 # It is assumed that this module is installed as a dependency to a scribe plug-in
 
-CWD=`pwd`;
-DIRNAME=`dirname $0`;
+CWD=`pwd`
+DIRNAME=`dirname $0`
+TARGET_PATH="$CWD/../run-tests.sh"
 
-if [ ! -e "$CWD/run-tests.sh"  ]; then
-  cp "$DIRNAME/run-tests.sh"  "$CWD/run-tests.sh";
+if [ ! -e $TARGET_PATH  ]
+then
+  cp "$DIRNAME/run-tests.sh" $TARGET_PATH
+  echo "Copying run-tests.sh to $TARGET_PATH: cp result $?"
 fi
