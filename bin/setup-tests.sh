8c84546eb20b4ad52e5e04f0ae773289b234a0ca
Robert Rees <robert.rees@guardian.co.uk>
2015-10-08 12:12:14 +0100

    Updates the Selenium versions in the helpers

--- bin/setup-tests.sh
+++ bin/setup-tests.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Download the selenium JAR
-SELENIUM_VERSION=2.46.0
-SELENIUM_MINOR_VERSION=2.46
+SELENIUM_VERSION=2.48.0
+SELENIUM_MINOR_VERSION=2.48
 
 LOCAL_SELENIUM=vendor/selenium-server-standalone-$SELENIUM_VERSION.jar
 
